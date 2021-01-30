6806aae76ea53b8ab1d786846e40e90447b40a98
PhyoZawHein <phyozawhein1997@gmail.com>
2021-01-29 21:54:00 -0500

    Fixed the authentication

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {AuthProvider} from './contexts/AuthContext';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
