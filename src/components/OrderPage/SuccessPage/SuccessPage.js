56927a13e9ed78e961c34aa92620335d93b20374
Ravid Rahman <ravidrahman@gmail.com>
2021-01-31 04:34:13 -0500

    fixed checkout page and success page ui; fixed other minor ui issues

--- src/components/OrderPage/SuccessPage/SuccessPage.js
+++ src/components/OrderPage/SuccessPage/SuccessPage.js
@@ -1,11 +1,45 @@
 import React, { useEffect} from 'react';
-import Button from 'react-bootstrap/Button'
+import Button from '@material-ui/core/Button';
 import { useAuth } from '../../../contexts/AuthContext';
 import Fire from '../../../firebaseConfig';
 import firebase from 'firebase/app';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9,
+      marginTop:'30'
+    },
+    container: {
+        margin: 'auto',
+        maxWidth: '90vw',
+        [theme.breakpoints.up('sm')]: {
+            maxWidth: '50vw'
+        }
+    }
+  }));
 
 export default function SuccessPage (props){
+
+    const classes = useStyles();
+
             const {currentUser}= useAuth();
             const {checkoutvalues} = props;
 
@@ -113,11 +147,9 @@ export default function SuccessPage (props){
                 }
                
                 return(
-                    <div className="CheckOut">
-                    <h1>Thank you very much !</h1>
-                    <div>
-                    <Button variant="primary"  name="step" value={0} onClick={next}>Order Again</Button>
-                    </div>
-                </div>
+                    <Container className = {classes.container}>
+                        <Typography variant="h5" component="h1"><strong>Thank you very much!</strong></Typography><br></br>
+                        <Button color="primary" size="small" onClick={next}>Order Again</Button>
+                    </Container>
                 )
             }
